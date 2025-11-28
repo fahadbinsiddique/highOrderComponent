5d3aca50276568e83314257c013269bbe96aa55e
fahadbinsiddique <fahad.bin.siddique345@gmail.com>
2025-11-28 18:26:27 +0600

    Refactor MyInput component to use useImperativeHandle for focus control

--- src/Components/MyInput.jsx
+++ src/Components/MyInput.jsx
@@ -1,10 +1,26 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const MyInput = (props, ref) => {
+  const realInputRef = useRef(null);
+
+  useImperativeHandle(ref, () => ({
+    focus() {
+      realInputRef.current.focus();
+    },
+  }));
+
+  //   useImperativeHandle(ref, () => {
+  //     return {
+  //       focus() {
+  //         realInputRef.current.focus();
+  //       },
+  //     };
+  //   });
+
   return (
     <input
       {...props}
-      ref={ref}
+      ref={realInputRef}
       className="bg-gray-400 m-10 border-2 p-5 border-sky-500"
     />
   );
